dd8f132c07eb2a8d9b6923c547212251b518645c
Josef Pihrt <josef.pihrt@outlook.com>
2019-12-16 13:07:34 +0100

    Update build scripts

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,11 +1,20 @@
 @echo off
 
+dotnet restore --force "..\src\Orang.sln"
+
 "%ProgramFiles%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild" "..\src\Orang.sln" ^
  /t:Clean,Build ^
  /p:Configuration=Release,RunCodeAnalysis=false ^
  /nr:false ^
  /v:normal ^
  /m
+
+if errorlevel 1 (
+ pause
+ exit
+)
+
+dotnet test -c Release --no-build "..\src\Tests\CommandLine.Tests\CommandLine.Tests.csproj"
 
 if errorlevel 1 (
  pause
---
+++ tools/clean_bin_obj.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+orang delete "../src" -a d -n "bin|obj" wi --content-only -y summary
+
+pause

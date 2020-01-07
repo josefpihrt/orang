41e432a7a3ad5886489a6e76c151deea30a84fa5
Josef Pihrt <josef.pihrt@outlook.com>
2020-01-07 16:02:58 +0100

    Update script

--- tools/roslynator_find_unused_symbols.cmd
+++ tools/roslynator_find_unused_symbols.cmd
@@ -5,7 +5,7 @@ set _msbuildPath="C:\Program Files\Microsoft Visual Studio\2019\Community\MSBuil
 
 %_msbuildPath%\msbuild "%_roslynatorPath%\CommandLine.sln" /t:Build /p:Configuration=Debug /v:m /m
 
-"%_roslynatorPath%\CommandLine\bin\Debug\net472\roslynator" find-symbols "E:\Dokumenty\Visual Studio 2017\Projects\Orang\src\Orang.sln" ^
+"%_roslynatorPath%\CommandLine\bin\Debug\net472\roslynator" find-symbols "..\src\Orang.sln" ^
  --msbuild-path %_msbuildPath% ^
  --visibility public internal private ^
  --symbol-groups type member ^

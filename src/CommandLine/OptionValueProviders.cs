ce654733d86e0c5148c3312a8ad24aa3e5702984
Josef Pihrt <josef.pihrt@outlook.com>
2021-01-04 02:49:02 +0100

    Update description

--- src/CommandLine/OptionValueProviders.cs
+++ src/CommandLine/OptionValueProviders.cs
@@ -539,7 +539,7 @@ namespace Orang.CommandLine
 
         public static OptionValueProvider PipeMode { get; } = new OptionValueProvider(
             MetaValues.PipeMode,
-            SimpleOptionValue.Create(CommandLine.PipeMode.Text, description: "Use redirected input as a text to be searched (default)."),
+            SimpleOptionValue.Create(CommandLine.PipeMode.Text, description: "Use redirected input as a text to be searched."),
             SimpleOptionValue.Create(CommandLine.PipeMode.Paths, description: "Use redirected input as a list of paths separated with newlines."));
 
         public static ImmutableDictionary<string, OptionValueProvider> ProvidersByName

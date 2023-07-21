1d06adf9388c16a577d1731176d19c5e68aa11e8
Josef Pihrt <josef@pihrt.net>
2023-07-21 12:49:54 +0200

    update

--- src/FileSystem.ApiTest/Program.cs
+++ src/FileSystem.ApiTest/Program.cs
@@ -5,7 +5,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using Orang;
 using Orang.FileSystem;
-using Orang.FileSystem.FileSystem;
 
 namespace N;
 
--- src/FileSystem/FileSystem/SearchBuilder.cs
+++ src/FileSystem/FileSystem/SearchBuilder.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
-using Orang.FileSystem.FileSystem.Operations;
+using Orang.FileSystem.Operations;
 
 #pragma warning disable RCS1223 // Mark publicly visible type with DebuggerDisplay attribute.
 

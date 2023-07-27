0c5595c6e062b802ad6138710b68a259b8ba1161
Josef Pihrt <josef@pihrt.net>
2023-07-27 11:56:22 +0200

    update

--- tools/generate_ref_docs.ps1
+++ tools/generate_ref_docs.ps1
@@ -3,8 +3,9 @@ $roslynatorExe="../../roslynator/src/CommandLine/bin/Debug/net7.0/Roslynator"
 dotnet restore "../../Roslynator/src/CommandLine.sln" /p:Configuration=Debug -v m
 dotnet build "../../Roslynator/src/CommandLine.sln" --no-restore /p:Configuration=Debug /v:m /m
 
-& $roslynatorExe generate-doc "../src/FileSystem/FileSystem.csproj" `
+& $roslynatorExe generate-doc "../src/Orang.sln" `
  --properties "Configuration=Release" `
+ --projects "Core" "FileSystem" `
  --heading ".NET API Reference" `
  -o "build/ref" `
  --host docusaurus `
@@ -13,8 +14,9 @@ dotnet build "../../Roslynator/src/CommandLine.sln" --no-restore /p:Configuratio
  --ignored-root-parts all `
  --max-derived-types 10
 
-& $roslynatorExe generate-doc-root "../src/FileSystem/FileSystem.csproj" `
+& $roslynatorExe generate-doc-root "../src/Orang.sln" `
  --properties "Configuration=Release" `
+ --projects "Core" "FileSystem" `
  -o "build/ref.md" `
  --host docusaurus `
  --heading ".NET API Reference" `

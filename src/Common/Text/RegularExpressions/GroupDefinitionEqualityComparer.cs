3a4c570e9777c06f6147db81aa20b3eb9c8c40f3
Josef Pihrt <josef@pihrt.net>
2023-11-22 05:33:05 +0100

    update

--- src/Common/Text/RegularExpressions/GroupDefinitionEqualityComparer.cs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using System.Collections.Generic;
-
-namespace Orang.Text.RegularExpressions;
